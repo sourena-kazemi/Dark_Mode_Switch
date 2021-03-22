06adc4dd891c7a229c203f78bcc39c9a24226169
Your Namesourena-kazemi$ git config --global user.name Your <kazemisourena@gmail.com>
2021-03-22 18:25:48 +0430

    v1.0.0

---
+++ index.js
@@ -0,0 +1,30 @@
+var dark_mode = false;
+var root = document.querySelector(":root")
+var checkbox_item = document.getElementsByClassName("checkbox__item")[0];
+var checkbox_logo = document.getElementsByTagName("img")[0];
+
+function checkbox(){
+switch (dark_mode){
+	case false:{
+		dark_mode=true;
+		root.style.setProperty("--bgc","#383838");
+		root.style.setProperty("--tc","#FFFFFF");
+		root.style.setProperty("--cc","#E2E2E2");
+		root.style.setProperty("--cic","#555555");
+		checkbox_item.style.left="60px";
+		checkbox_logo.src="images/icons8-crescent-moon-100.svg";
+		break;
+	}
+
+	case true:{
+		dark_mode=false;
+		root.style.setProperty("--bgc","#F6F6F6");
+		root.style.setProperty("--tc","#000000");
+		root.style.setProperty("--cc","#555555");
+		root.style.setProperty("--cic","#E2E2E2");
+		checkbox_item.style.left="0";
+		checkbox_logo.src="images/icons8-summer-100.svg";
+		break;
+	}
+}
+}
